8cd5b6f6000eb727dfc97350b1ad5a60dd674ed3
Sabuz <everythingkst@gmail.com>
2025-03-15 15:34:32 +0600

    second commit

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -5,22 +5,27 @@ use App\Calculator;
 
 class CalculatorTest extends TestCase
 {
-    // public function testAddition()
-    // {
-    //     $calculator = new Calculator();
-    //     $this->assertEquals(5, $calculator->add(2, 3));
-    // }
+    public function testAddition()
+    {
+        $calculator = new Calculator();
+        $this->assertEquals(5, $calculator->add(2, 3));
+    }
 
-    // public function testSubtraction()
-    // {
-    //     $calculator = new Calculator();
-    //     $this->assertEquals(2, $calculator->subtract(5, 3));
-    // }
+    public function testSubtraction()
+    {
+        $calculator = new Calculator();
+        $this->assertEquals(2, $calculator->subtract(5, 3));
+    }
 
-    // public function testMultiplication(){
-    //     $calculator = new Calculator();
-    //     $this->assertEquals(6, $calculator->multiply(2, 3));
-    // }
+    public function testMultiplication(){
+        $calculator = new Calculator();
+        $this->assertEquals(6, $calculator->multiply(2, 3));
+    }
+
+    public function testDivision(){
+        $calculator = new Calculator();
+        $this->assertEquals(2, $calculator->divide(6, 3));
+    }
 
     // suppose i want to test an object whether it is an instance of a Calculator class or not
     public function testInstanceOf(){
